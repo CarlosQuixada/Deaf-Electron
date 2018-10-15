c0af7e92a25eaf8b623f13f5d10f1eafbd447508
CarlosQuixada <carlosquixada95@gmail.com>
2018-10-14 22:55:23 -0300

    Modulos-Produto/Cliente/Venda

---
+++ controller/cliente.js
@@ -0,0 +1,58 @@
+var read = require('read-file-utf8');
+var loki = require('lokijs');
+var fileExists = require('file-exists'); 
+var db = new loki(__dirname+'/db.json');
+var data = {};
+
+var exists = fileExists.sync(__dirname+'/db.json')
+
+if(exists === true){
+  data = read(__dirname+'/db.json');
+  db.loadJSON(data);
+}else{
+  db.addCollection('vendas');
+  db.addCollection('clientes');
+  db.addCollection('produtos');
+  db.save();
+}
+
+window.Vue = require('vue');
+var clientes =db.getCollection('clientes');
+
+new Vue({
+  el:'body',
+  data:{
+    mode:'',
+    clientes:[],
+    cliente_update:'',
+    client:{
+      nome:'',
+      telefone:''
+    }
+  },
+  ready:function(){
+    this.clientes = clientes.data;
+  },
+  methods:{
+    editClient:function(client){
+      this.mode = 'edicao';
+      this.openModal = true;
+      this.client = client;
+      this.cliente_update = client;
+    },
+    clientStroreOrUpdate:function(){
+      if(typeof this.client.$loki != 'undefined'){
+        clientes.update(this.client);
+        this.mode = '';
+        this.client={nome:'',telefone:''};
+        this.cliente_update='';
+      }else{
+        clientes.insert(this.client);
+        this.mode = '';
+        this.client={nome:'',telefone:''};
+        this.cliente_update ='';
+      }
+      db.save();
+    }
+  }
+});
---
+++ controller/venda.js
@@ -0,0 +1,66 @@
+var read = require('read-file-utf8');
+var loki = require('lokijs');
+var fileExists = require('file-exists'); 
+var db = new loki(__dirname+'/db.json');
+var data = {};
+
+var exists = fileExists.sync(__dirname+'/db.json')
+
+if(exists === true){
+  data = read(__dirname+'/db.json');
+  db.loadJSON(data);
+}else{
+  db.addCollection('vendas');
+  db.addCollection('clientes');
+  db.addCollection('produtos');
+  db.save();
+}
+
+window.Vue = require('vue');
+var produtos =db.getCollection('produtos');
+var vendas = db.getCollection('vendas');
+var clientes = db.getCollection('clientes');
+
+new Vue({
+  el:'body',
+  data:{
+    mode:'',
+    produtos:{},
+    vendas:{},
+    clientes:{},
+    sale_update:'',
+    sale:{
+      cliente:'',
+      produto:'',
+      preco:0,
+      qtd:1
+    }
+  },
+  ready:function(){
+    this.produtos = produtos.data;
+    this.vendas = vendas.data;
+    this.clientes = clientes.data;
+  },
+  methods:{
+    editSale:function(sale){
+      this.mode = 'edicao';
+      this.sale = sale;
+      this.sale_update = sale;
+    },
+    saleStroreOrUpdate:function(){
+      if(typeof this.sale.$loki != 'undefined'){
+        vendas.update(this.sale);
+        this.mode = '';
+        this.sale={cliente:this.sale.cliente,produto:'',preco:0,qtd:1}
+        this.sale_update ='';
+      }else{
+        this.sale.preco = produtos.find({nome:this.sale.produto})[0].preco;
+        vendas.insert(this.sale);
+        this.mode='';
+        this.sale={cliente:this.sale.cliente,produto:'',preco:0,qtd:1}
+        this.sale_update ='';
+      }
+      db.save();
+    }
+  }
+});
